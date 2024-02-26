65fa34e26dfb90aaecf2d105e09655866ba3192b
Illia Val <illia.val.777@gmail.com>
2024-02-25 21:45:37 -0600

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
-# CTA-Train-Tracker-API-Key
+# Chicago CTA Bus Tracker
+
+An app to display the Chicago Transit Authority (CTA) bus locations around the Chicago area on a Google Map. By Clicking on a Bus Route markers will be added to the Google Map showing the bus locations for that route, and by clicking again on the route name you can refresh the bus locations as they buses move around the route.
+
+
+
+__Project Status - Completed: 01/15/2018__
+
+----
+
+
+### Table of Contents
+
+  -  Description
+  -  What I Learned From The Exercise
+  -  Website Technologies Used
+  -  How to View This App
+  -  File and Directory Structure
+  -  Revision History
+
+----
+
+
+### Description
+An app to display the Chicago Transit Authority (CTA) bus locations around the Chicago area on a Google Map. By Clicking on a Bus Route markers will be added to the Google Map showing the bus locations for that route, and by clicking again on the route name you can refresh the bus locations as they buses move around the route.
+
+![Chicago CTA Bus Tracker Exercise](https://github.com/DKMitt/cta-buses-google-maps/blob/master/assets/img/ccbt.gif)
+
+----
+
+
+### What I Learned From The Exercise
+- To work with the Google API to fetch requested data.
+- Create a custom element that wraps a Google map.
+- Add markers to the Google map.
+- To become familiar with CanJS.
+
+----
+
+
+### Website Technologies Used
+
+Description of website technologies used to develop this app.
+
+- HTML
+- CSS
+- JavaScript
+- Can.JS
+
+----
+
+
+### How to Use This App
+
+How to use this App:
+
+1. Click a Bus Route.
+2. Explore the markers added to the Google Map showing the bus locations for that route.
+3. Click the route name buttons to refresh the buses current locations.
+
+   ##### [Demo - Click Here](http://www.dkmitt.com/mycoding/cta-buses-google-maps/)
+
+
+### File and Directory Structure
+
+```
+.
+├── assets
+│   │
+│   ├── css
+│   │   │
+│   │   └── style.css
+│   │
+│   ├── img
+│   │   │
+│   │   └── ccbt.gif
+│   │
+│   └── js
+│       │
+│       └── bustracker.js
+│ 
+├── .gitignore
+│
+├── index.html
+│
+└── README.md     
+       
+```
+
+----
+
+
+### Revision History 
+
+Description of revisions - __Completed: 01/15/2018__
+
+  - [x] Dev - file and directory structure creation  - 01/13/2018
+  - [x] Dev - project completion  - 01/15/2018
+
---
+++ assets/js/bustracker.js
@@ -0,0 +1,86 @@
+var proxyUrl =   "https://cors-anywhere.herokuapp.com/";
+var token = "?key=6pAh8tTMx7TBtdEV5hpEbZTWb&format=json";
+var apiRoot = "http://www.ctabustracker.com/bustime/api/v2/";
+var getRoutesEnpoint = apiRoot + "getroutes" + token;
+var getVehiclesEndpoint = apiRoot + "getvehicles" + token + "&vid=1295&tmres=s&format=json";
+
+var BusTrackerVM = can.DefineMap.extend({
+  title: {
+   value:"Chicago CTA Bus Tracker by Val Illia"
+  },
+  routesPromise: {
+    value() {
+      return fetch(proxyUrl + getRoutesEnpoint, {
+          method: 'GET',
+          mode: 'no-cors'
+      })
+        .then(response => response.json())
+        .then(data => data["bustime-response"].routes);
+  }
+ },
+ route: 'any',
+ vehiclesPromise: 'any',
+ pickRoute(route) {
+   this.route = route;
+   this.vehiclesPromise = fetch(proxyUrl + getVehiclesEndpoint + "&rt=" + route.rt, {
+       method: 'GET',
+       mode: 'no-cors'
+   })
+     .then(response => response.json())
+     .then(data => {
+       if (data["bustime-response"].error) {
+         return Promise.reject(data["bustime-response"].error[0]);
+       } else {
+         return data["bustime-response"].vehicle;
+       }
+     });
+  }
+});
+
+can.Component.extend({
+  tag: "google-map-view",
+  view: can.stache(`<div class='gmap'></div>`),
+  ViewModel: {
+    map: 'any',
+    vehicles: 'any',
+    markers: 'any'
+  },
+  events: {
+    "{viewModel} vehicles": function(vm, ev, newVehicles) {
+      if (Array.isArray(this.markers)) {
+        this.markers.forEach(marker => {
+          marker.setMap(null);
+        });
+        this.markers = null;
+      }
+      if ( newVehicles ) {
+        this.markers = newVehicles.map(vehicle => {
+           return new google.maps.Marker({
+             position: {
+               lat: parseFloat(vehicle.lat),
+               lng: parseFloat(vehicle.lon)
+             },
+             map: this.viewModel.map
+           });
+         });
+       }
+     },
+    "{element} inserted": function() {
+      googleAPI.then(() => {
+        this.viewModel.map = new google.maps.Map(this.element.firstChild, {
+         zoom: 10,
+         center: {
+           lat: 41.881,
+           lng: -87.623
+         }
+       });
+     });
+  }
+ }
+});
+
+var viewModel = new BusTrackerVM();
+
+var view = can.stache.from("app-view");
+var frag = view(viewModel);
+document.body.appendChild(frag);
